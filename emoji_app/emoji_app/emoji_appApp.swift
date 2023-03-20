17affb02d1924a50de0dc852880e01535fa7e8fa
imugikk <ugyparaptughessa@gmail.com>
2023-03-20 13:48:53 +0700

    first

---
+++ emoji_app/emoji_app/emoji_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  emoji_appApp.swift
+//  emoji_app
+//
+//  Created by Paraptughessa Premaswari on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct emoji_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
